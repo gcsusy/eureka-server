b502090b87cf73e6d957388568a95c61fd0aebda
gcsusy <gcsusy@gmail.com>
2025-02-25 17:30:53 -0600

    first commit

---
+++ src/test/java/gc/unir/actividad2/server_eureka/ServerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package gc.unir.actividad2.server_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
